e175942df5da388e631a5152675d99017c78965a
dmn2-png <dmn2@aluno.ifal.edu.br>
2026-09-09 16:06:02 -0300

    completo

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // TODO: Adicione o middleware express.json() para parsear o body
 // POST /echo → 200, devolve o mesmo JSON recebido no body
 // O Content-Type da resposta deve ser application/json
+
+app.post('/echo', (req, res) => {
+    res.status(200).json(req.body);
+});
+
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
